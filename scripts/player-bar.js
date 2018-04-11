b3491199a30c2443b07bbe8af3b9a586f0ae472b
Apdet <deterding1.a@gmail.com>
2018-04-10 20:26:46 -0500

    added variable to totaltime

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -33,6 +33,7 @@ $(document).ready(function(){
    setInterval( () => {
      if (player.playState !== 'playing') { return; }
      const currentTime = player.getTime();
+     const totalTime = player.getTime();
      const duration = player.getDuration();
      const percent = (currentTime / duration) * 100;
      $('#time-control .current-time').text( currentTime );
